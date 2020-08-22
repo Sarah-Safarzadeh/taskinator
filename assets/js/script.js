d1e763a83d1f69d692469c08a90eeee3b5019d40
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-08-22 14:15:55 -0500

    updates to taskhandler fucntion

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,29 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandler = function(event) {
+var createTaskHandler = function (event) {
   event.preventDefault();
 
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  };
-
-formEl.addEventListener("submit", createTaskHandler);
+  var taskNameInput = document.querySelector("select[name='task-type']").value;
+  // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
+  //create div to hold task info and add to list item
+var taskInfoEl = document.createElement("div");
+// gives it a class name:
+taskInfoEl = document.className = "task-info";
+// adds HTML content to the div:
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+listItemEl.appendChild(taskInfoEl);
+
+// Adds the entire list item to list
+tasksToDoEl.appendChild(listItemEl);
+ 
+};
+
+formEl.addEventListener("submit", createTaskHandler);
+var listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
+listItemEl.textContent = "This is a new task.";
+tasksToDoEl.appendChild(listItemEl);
